576ca977af30a28436d18164d4c94760469f70e1
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-06-14 10:12:24 +0200

    first commit

---
+++ login.py
@@ -0,0 +1,13 @@
+# Brent Lee Johnson ==> Class 1
+# End of Module Project
+# Lottery Numbers
+from tkinter import *
+from playsound import playsound
+
+root = Tk()
+root.title("Lotto Login Form")
+root.geometry("550x500")
+root.config(bg="#100C08")
+
+
+root.mainloop()

ae3140e94e2ef9c4cbd14de2783b187a8c7d4a1f
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-06-14 15:53:52 +0200

    Update

--- lotto.py
+++ lotto.py
@@ -5,7 +5,15 @@ from playsound import playsound
 root = Tk()
 root.title("Lotto Streak!")
 root.geometry("550x500")
-root.config(bg="#100C08")
+root.config(bg="#343434")
+
+# Heading
+lotto_lbl = Label(root, text="Lotto Streak!!", fg="#7A7A7A")
+lotto_lbl.config(bg="#343434")
+lotto_lbl.pack()
+
+# Content
+
 
 
 # Run the code

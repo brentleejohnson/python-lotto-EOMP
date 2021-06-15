fea381ceb637384f0d14824e0abf12685da45263
Brent Johnson <brentleejohnson73@gmail.com>
2021-06-15 15:19:13 +0200

    Fixing layout

--- login.py
+++ login.py
@@ -19,44 +19,44 @@ root.config(bg="#343434")
 # Heading
 registration_lbl1 = Label(root, text="Welcome To The Lotto Streak!!", fg="#7A7A7A")
 registration_lbl1.config(bg="#343434")
-registration_lbl1.pack()
+registration_lbl1.place(x="60", y="0")
 
 registration_lbl2 = Label(root, text="To play, please sign-in below:", fg="#7A7A7A")
 registration_lbl2.config(bg="#343434")
-registration_lbl2.pack()
+registration_lbl2.place(x="60", y="20")
 
 # Content
 # Name
 name_lbl = Label(root, text="Your Name:", fg="#7A7A7A")
 name_lbl.config(bg="#343434")
-name_lbl.pack()
+name_lbl.place(x="0", y="50")
 
 name_entry = Entry(root)
 name_entry.config()
-name_entry.pack()
+name_entry.place(x="70", y="50")
 
 # Email
 email_lbl = Label(root, text="Email:", fg="#7A7A7A")
 email_lbl.config(bg="#343434")
-email_lbl.pack()
+email_lbl.place(x="0", y="80")
 
 email_entry = Entry(root)
 email_entry.config()
-email_entry.pack()
+email_entry.place(x="70", y="80")
 
 # Id No.
-id_lbl = Label(root, text="Id No.:", fg="#7A7A7A")
+id_lbl = Label(root, text="Id No:", fg="#7A7A7A")
 id_lbl.config(bg="#343434")
-id_lbl.pack()
+id_lbl.place(x="0", y="110")
 
 id_entry = Entry(root)
 id_entry.config()
-id_entry.pack()
+id_entry.place(x="70", y="110")
 
 # Footer
 footer_lbl = Label(root, text="Made by Brent Johnson", fg="#7A7A7A")
 footer_lbl.config(bg="#343434")
-footer_lbl.pack()
+footer_lbl.place(x="85", y="280")
 
 
 # Run the code

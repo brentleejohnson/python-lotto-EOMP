d380253bc60876f5ce9915bf161bc79696c402a3
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-06-17 16:11:37 +0200

    Fixing up layout

--- lotto.py
+++ lotto.py
@@ -21,48 +21,48 @@ lotto_input_spin_1.place(x=20, y=70)
 
 lotto_input_spin_2 = Spinbox(root, from_=0, to=49)
 lotto_input_spin_2.config(width=2, font=("Garuda", 20))
-lotto_input_spin_2.place(x=120, y=70)
+lotto_input_spin_2.place(x=120, y=90)
 
 lotto_input_spin_3 = Spinbox(root, from_=0, to=49)
 lotto_input_spin_3.config(width=2, font=("Garuda", 20))
-lotto_input_spin_3.place(x=220, y=70)
+lotto_input_spin_3.place(x=220, y=110)
 
 lotto_input_spin_4 = Spinbox(root, from_=0, to=49)
 lotto_input_spin_4.config(width=2, font=("Garuda", 20))
-lotto_input_spin_4.place(x=320, y=70)
+lotto_input_spin_4.place(x=320, y=130)
 
 lotto_input_spin_5 = Spinbox(root, from_=0, to=49)
 lotto_input_spin_5.config(width=2, font=("Garuda", 20))
-lotto_input_spin_5.place(x=420, y=70)
+lotto_input_spin_5.place(x=420, y=150)
 
 lotto_input_spin_6 = Spinbox(root, from_=0, to=49)
 lotto_input_spin_6.config(width=2, font=("Garuda", 20))
-lotto_input_spin_6.place(x=520, y=70)
+lotto_input_spin_6.place(x=520, y=170)
 
 # Answers
-lotto_output_entry_1 = Entry(root)
-lotto_output_entry_1.config(width=10)
-lotto_output_entry_1.place(x=0, y=200)
+lotto_output_entry_1 = Entry(root, state="disable")
+lotto_output_entry_1.config(width=4, font=("Garuda", 15))
+lotto_output_entry_1.place(x=20, y=200)
 
-lotto_output_entry_2 = Entry(root)
-lotto_output_entry_2.config(width=10)
-lotto_output_entry_2.place(x=100, y=200)
+lotto_output_entry_2 = Entry(root, state="disable")
+lotto_output_entry_2.config(width=4, font=("Garuda", 15))
+lotto_output_entry_2.place(x=120, y=220)
 
-lotto_output_entry_3 = Entry(root)
-lotto_output_entry_3.config(width=10)
-lotto_output_entry_3.place(x=200, y=200)
+lotto_output_entry_3 = Entry(root, state="disable")
+lotto_output_entry_3.config(width=4, font=("Garuda", 15))
+lotto_output_entry_3.place(x=220, y=240)
 
-lotto_output_entry_4 = Entry(root)
-lotto_output_entry_4.config(width=10)
-lotto_output_entry_4.place(x=300, y=200)
+lotto_output_entry_4 = Entry(root, state="disable")
+lotto_output_entry_4.config(width=4, font=("Garuda", 15))
+lotto_output_entry_4.place(x=320, y=260)
 
-lotto_output_entry_5 = Entry(root)
-lotto_output_entry_5.config(width=10)
-lotto_output_entry_5.place(x=400, y=200)
+lotto_output_entry_5 = Entry(root, state="disable")
+lotto_output_entry_5.config(width=4, font=("Garuda", 15))
+lotto_output_entry_5.place(x=420, y=280)
 
-lotto_output_entry_6 = Entry(root)
-lotto_output_entry_6.config(width=10)
-lotto_output_entry_6.place(x=500, y=200)
+lotto_output_entry_6 = Entry(root, state="disable")
+lotto_output_entry_6.config(width=4, font=("Garuda", 15))
+lotto_output_entry_6.place(x=520, y=300)
 
 # Buttons
 # Claim Button

d05fc5f1ae8c605220e27674d412987c12bc8c41
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-06-18 13:14:30 +0200

    Reorganising layouts

--- lotto.py
+++ lotto.py
@@ -18,35 +18,29 @@ lotto_lbl.pack()
 
 # Content
 # Input
-lotto_input_spin_1 = tk.Spinbox(root, from_=0, to=49)
-lotto_input_spin_1.config(width=2, font=("Garuda", 20))
-lotto_input_spin_1.place(x=150, y=70)
+lotto_input_entry_1 = Entry(root, from_=0, to=49)
+lotto_input_entry_1.config(width=2, font=("Garuda", 20))
+lotto_input_entry_1.place(x=150, y=70)
 
-lotto_input_spin_2 = Spinbox(root, from_=0, to=49)
-lotto_input_spin_2.config(width=2, font=("Garuda", 20))
-lotto_input_spin_2.place(x=225, y=70)
+lotto_input_entry_2 = Entry(root, from_=0, to=49)
+lotto_input_entry_2.config(width=2, font=("Garuda", 20))
+lotto_input_entry_2.place(x=225, y=70)
 
-lotto_input_spin_3 = Spinbox(root, from_=0, to=49)
-lotto_input_spin_3.config(width=2, font=("Garuda", 20))
-lotto_input_spin_3.place(x=300, y=80)
+lotto_input_entry_3 = Entry(root, from_=0, to=49)
+lotto_input_entry_3.config(width=2, font=("Garuda", 20))
+lotto_input_entry_3.place(x=300, y=80)
 
-lotto_input_spin_4 = Spinbox(root, from_=0, to=49)
-lotto_input_spin_4.config(width=2, font=("Garuda", 20))
-lotto_input_spin_4.place(x=370, y=120)
+lotto_input_entry_4 = Entry(root, from_=0, to=49)
+lotto_input_entry_4.config(width=2, font=("Garuda", 20))
+lotto_input_entry_4.place(x=370, y=120)
 
-lotto_input_spin_5 = Spinbox(root, from_=0, to=49)
-lotto_input_spin_5.config(width=2, font=("Garuda", 20))
-lotto_input_spin_5.place(x=390, y=190)
+lotto_input_entry_5 = Entry(root, from_=0, to=49)
+lotto_input_entry_5.config(width=2, font=("Garuda", 20))
+lotto_input_entry_5.place(x=390, y=190)
 
-lotto_input_spin_6 = Spinbox(root, from_=0, to=49)
-lotto_input_spin_6.config(width=2, font=("Garuda", 20))
-lotto_input_spin_6.place(x=395, y=260)
-
-
-# Answers
-answ_frame = Frame(root, width=320, height=140)
-answ_frame.config()
-answ_frame.place(x=20, y=165)
+lotto_input_entry_6 = Entry(root, from_=0, to=49)
+lotto_input_entry_6.config(width=2, font=("Garuda", 20))
+lotto_input_entry_6.place(x=395, y=260)
 
 
 # Amount of Plays Left
@@ -66,21 +60,49 @@ print(lotto_numbers)
 def lotto():
     # Getting users' inputs
     try:
-        user_list = [int(lotto_input_spin_1.get()), int(lotto_input_spin_2.get()),
-                     int(lotto_input_spin_3.get()), int(lotto_input_spin_4.get()),
-                     int(lotto_input_spin_5.get()), int(lotto_input_spin_6.get())]
+        user_list = [int(lotto_input_entry_1.get()), int(lotto_input_entry_2.get()),
+                     int(lotto_input_entry_3.get()), int(lotto_input_entry_4.get()),
+                     int(lotto_input_entry_5.get()), int(lotto_input_entry_6.get())]
 
         for i in range(6):
             if 0 <= int(user_list[i]) <= 49:
                 user_list.append(user_list[i])
             elif 49 < int(user_list[i]):
                 messagebox.showerror("Error", "Enter numbers between 0-49")
-            # Supposed to add the ouput to output_entries
+            frame_lbl_1 = Label(Frame, text=lotto_numbers, font=("Garuda", 15))
+            frame_lbl_1.pack()
 
         # Storage for the lists
-        matching_numbers = set(user_list) & set(lotto_numbers)
-    except:
-        messagebox.show('error')
+        matched_numbers = set(user_list) & set(lotto_numbers)
+
+        if user_list == user_list:
+            matched_numbers_lbl = Label(Frame, text=matched_numbers, font=("Garuda", 15))
+            matched_numbers_lbl.pack()
+        else:
+            if user_list == 00:
+                messagebox.showerror("Try Again", "There aren't any matched numbers:(")
+
+        if len(matched_numbers) == 0:
+            matching_label = Label(Frame, text="0", font=("Garuda", 15))
+            matching_label.pack()
+            messagebox.showerror("Try Again", "There aren't any matched numbers:(")
+
+        elif len(matched_numbers) == 1:
+            messagebox.showerror("Tough Luck", "You only matched one number, try again")
+
+        elif len(matched_numbers) == 2:
+            messagebox.showinfo("Nice!", "You won R20.00")
+        elif len(matched_numbers) == 3:
+            messagebox.showinfo("NICE!!", "You won R100.50")
+        elif len(matched_numbers) == 4:
+            messagebox.showinfo("Amazing!", "You won R384.00")
+        elif len(matched_numbers) == 5:
+            messagebox.showinfo("FABULOUS!!", "You won R8 584.00")
+        elif len(matched_numbers) == 6:
+            messagebox.showinfo("Congratulations Champion!", "You won R10 000 000.00")
+
+    except ValueError:
+        messagebox.showerror("Enter your numbers to play")
 
 
 # Buttons
@@ -90,7 +112,7 @@ lotto_claim_btn.config()
 lotto_claim_btn.place(x=5, y=465)
 
 # Play Again Button
-lotto_play_again_btn = Button(root, text="PLAY AGAIN")
+lotto_play_again_btn = Button(root, text="PLAY", command=lotto)
 lotto_play_again_btn.config()
 lotto_play_again_btn.place(x=180, y=465)
 
